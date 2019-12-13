ac220813944af5b491333701918e099f9dfea3f8
Nathan Fish <lordcirth@gmail.com>
2019-12-13 15:52:11 -0500

    try haproxy -> apache in vbox

--- nixops/top.nix
+++ nixops/top.nix
@@ -1,7 +1,9 @@
 {
   defaults = {
-    imports = [ ./ssh.nix ];
+    imports = [ ./vbox.nix ];
   };
 
   haproxy = import ./haproxy.nix;
+
+  web01 = import ./web.nix;
 }
--- nixops/vbox.nix
+++ nixops/vbox.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 {
   deployment.targetEnv = "virtualbox";
-  deployment.virtualbox.memorySize = 1024; # megabytes
-  deployment.virtualbox.vcpu = 2; # number of cpus
+  deployment.virtualbox = {
+    memorySize = 1024;
+    vcpu = 2;
+    headless = true;
+  };
 }
