0e6c26627eb9769e703aff9798d07afe52c5f970
Nathan Fish <lordcirth@gmail.com>
2019-12-16 12:04:43 -0500

    silence binfmt warning

--- nixops/vbox.nix
+++ nixops/vbox.nix
@@ -6,4 +6,10 @@
     vcpu = 2;
     headless = true;
   };
+
+  # https://github.com/NixOS/nixops/issues/574#issuecomment-443540177
+  systemd.additionalUpstreamSystemUnits =
+  [ "proc-sys-fs-binfmt_misc.automount"
+    "proc-sys-fs-binfmt_misc.mount"
+  ];
 }
