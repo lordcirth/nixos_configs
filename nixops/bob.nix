70dfb993e63a9c4ede2fe6fe81ecb54594fada2b
Nathan Fish <lordcirth@gmail.com>
2020-01-08 13:41:13 -0500

    refactor user 'bob' into separate file; create laptop.nix

---
+++ nixops/bob.nix
@@ -0,0 +1,9 @@
+{
+  users.users.bob = {
+    uid = 1020;
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    hashedPassword =
+      "$6$OsQZFtbN$7xzZ.G5Sm93dioYHsWvUd3Zhyx7.b3FM0gYYnh1kwIhA1bjuo/jy2PCLLtagEL2iieb4.lzYcvGy9Mzj5BuGV0";
+  };
+}
