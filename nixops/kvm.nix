c64339a4413c50f6eefbf8eb905c49d74e41d025
Nathan <lordcirth@gmail.com>
2020-03-13 23:17:29 -0400

    kvm: add ipfs

--- nixops/kvm.nix
+++ nixops/kvm.nix
@@ -5,5 +5,11 @@
   kvm = {
     deployment.targetHost = "192.168.1.222";
     services.qemuGuest.enable = true;
+    networking.firewall.enable = false;
+    services.ipfs = {
+      enable          = true;
+      apiAddress      = "/ip4/192.168.1.222/tcp/5001";
+      gatewayAddress  = "/ip4/192.168.1.222/tcp/8080";
+    };
   };
 }
