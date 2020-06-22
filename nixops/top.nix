ec05952cdda7f56ef88e20c1c976402a1bfe1b4c
Nathan <lordcirth@gmail.com>
2020-06-22 16:10:31 -0400

    cleanup top

--- nixops/top.nix
+++ nixops/top.nix
@@ -2,18 +2,9 @@
   network = { rollback = true; };
   defaults = { imports = [ ./all.nix ./bob.nix ./kvm.nix ]; };
 
-  kvm = {
+  dev = {
     deployment.targetHost = "192.168.1.222";
-    services.ipfs = {
-      enable          = true;
-      apiAddress      = "/ip4/192.168.1.222/tcp/5001";
-      gatewayAddress  = "/ip4/192.168.1.222/tcp/8080";
-    };
-
+    networking.firewall.enable = false;
+    imports = [ ./ipfs.nix ];
   };
-
-#  pxe = {
-#    deployment.targetHost = "192.168.1.248";
-#    imports = [ ./pxe.nix ];
-#  };
 }
